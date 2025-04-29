15e3c15f8d48748a3c431e406af4a7e86662882c
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-04-29 13:44:17 +0530

    Add cookie-parser and cors middleware; implement ApiError and ApiResponse classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
@@ -15,6 +15,16 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+console.log("MongoDB connection failed ",error);
+
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+    constructor(statusCode,
+        data,
+        message="success"
+    ){
+        this.message= message
+        this.data = data
+        this.statusCode=statusCode
+        this.success = statusCode
+    }
+}
