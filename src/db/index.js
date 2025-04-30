90bf65315bcd9767eb659764673530c3a4b50dc6
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-05-01 01:59:58 +0530

    all setup done

--- src/db/index.js
+++ src/db/index.js
@@ -14,3 +14,24 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+
+// PRACTISE 
+
+// import mongoose from "mongoose"
+// import { DB_NAME } from "../constants.js";
+
+
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        
+//     } catch (error) {
+//         console.log(error)
+//         process.exit(1)
+        
+//     }
+// }
+// export default connectDB;
--- src/index.js
+++ src/index.js
@@ -29,6 +29,28 @@ console.log("MongoDB connection failed ",error);
 
 
 
+// import dotenv from "dotenv"
+// import connectDB from "./db"
+
+// dotenv.config({
+//     path:"./.env"
+// })
+
+// connectDB
+// .then(()=>{
+//     app.listen((process.env.PORT || 8000),()=>{
+//         console.log("running at ",process.env.PORT)
+//     })
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+
+
+
+
+
 
 
 
