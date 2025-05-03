590d267b68549628672dfffdd4219731dac97710
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-05-04 02:34:40 +0530

    Implement user authentication: add login and logout functionality, JWT verification middleware, and token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,31 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+
+
+const generateAccessAndRefreshTokens = async(userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave : false})
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"Something when wrong while generating refresh and access toekn")
+    }
+}
+
+
+
+
+
+
+
 const registerUser = asyncHandler( async(req,res)=>{
     // res.status(200).json({
     //     message:"ok"
@@ -10,8 +35,8 @@ const registerUser = asyncHandler( async(req,res)=>{
     // console.log("FILES =>", req.files);
    // console.log("BODY  =>", req.body);
 //    console.log(req.files.avatar)
-console.log("Files:", req.files);
-console.log("Avatar Path:", req.files.avatar?.[0]?.path);
+// console.log("Files:", req.files);
+// console.log("Avatar Path:", req.files.avatar?.[0]?.path);
 
 
     // get user details front frontend
@@ -84,4 +109,92 @@ if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.lengt
    )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req,res)=>{
+        //req body ->data
+        //username or email
+        //find the user
+        //password check 
+        //access and refresh token
+        //send cookies and send responce that login successfull
+
+
+        const {username,email,password} = req.body;
+        if((!username && !email)){
+            throw new ApiError(400,"Username or email is required");
+        }
+
+        const user = await User.findOne({
+            $or: [{ username },{ email }]
+        });
+
+        if (!user) {
+            throw new ApiError(404,"User doesnt exist");
+        }
+
+        const isPasswordValid = await user.isPasswordCorrect(password);
+
+        if (!isPasswordValid) {
+            throw new ApiError(401,"Invalid user credentials ")
+        }
+
+        const  {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+        const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user:loggedInUser, accessToken , refreshToken
+                },
+                "User Logged In successfully"
+            )
+        )
+        
+
+
+})
+   
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+            {
+                new: true
+            }
+        
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User LoggedOut"))
+})
+
+
+
+
+
+
+export {registerUser,
+        loginUser,
+        logoutUser
+}
